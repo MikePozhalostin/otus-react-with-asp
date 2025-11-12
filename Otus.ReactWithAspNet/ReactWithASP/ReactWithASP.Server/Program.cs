9cbc832aa9cc1ed5f91268e719bdf91e60b3e55a
Михаил Пожалостин <m.pozhalostin@whyhappen.ru>
2025-11-12 12:19:25 +0300

    fix ports

--- Otus.ReactWithAspNet/ReactWithASP/ReactWithASP.Server/Program.cs
+++ Otus.ReactWithAspNet/ReactWithASP/ReactWithASP.Server/Program.cs
@@ -2,6 +2,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyAllowSpecificOrigins",
+                      policy =>
+                      {
+                          policy.WithOrigins("*")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -12,6 +24,8 @@ app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseCors("MyAllowSpecificOrigins");
 
 app.UseAuthorization();
 
